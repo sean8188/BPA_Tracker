8ef450d05e1f4088b368a67c1359d81714fba775
PS19SArnold <PS19SArnold@E2006PC41896>
2019-01-10 13:45:52 -0500

    First commit

--- BPA_Tracker/Pages/Students/Create.cshtml.cs
+++ BPA_Tracker/Pages/Students/Create.cshtml.cs
@@ -9,7 +9,7 @@ using BPA_Tracker.Models;
 
 namespace BPA_Tracker.Pages.Students
 {
-    public class CreateModel : EventNamePageModel  // Changed from PageModel
+    public class CreateModel : StudentEventsPageModel // Changed from PageModel
     {
         private readonly BPA_Tracker.Models.BPA_TrackerContext _context;
 
@@ -21,43 +21,53 @@ namespace BPA_Tracker.Pages.Students
         public IActionResult OnGet()
         {
             //LINE BELOW IS NEW
-            PopulateEventsDropDownList(_context);
+
+            var student = new Student();
+            student.AssignEvents = new List<AssignEvent>();
+
+            PopulateAssignedEventData(_context, student);
+            //PopulateEventsDropDownList(_context);
             return Page();
         }
 
         [BindProperty]
         public Student Student { get; set; }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCourses)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            //LINSE BELOW IS NEW*********************************************
-            var emptyCourse = new Student();
+            var newStudent = new Student();
+            if (selectedCourses != null)
+            {
+                newStudent.AssignEvents = new List<AssignEvent>();
+                foreach (var course in selectedCourses)
+                {
+                    var coursetoadd = new AssignEvent
+                    {
+                        EventID = int.Parse(course)
+                    };
+                    newStudent.AssignEvents.Add(coursetoadd);
+                }
+            }
 
             if (await TryUpdateModelAsync<Student>(
-                 emptyCourse,
-                 "student",   // Prefix for form value.
-                 s => s.StudentID, s => s.LastName, s => s.FirstName, s => s.StudentPhone, s => s.StudentEmail, s => s.ParentName, s => s.ParentPhone, s => s.ParentEmail))
+                newStudent,
+                "Student",
+                i => i.FirstName, i => i.LastName,
+                i => i.StudentPhone, i => i.StudentEmail,
+                i => i.ParentName, i => i.ParentPhone,
+                i => i.ParentEmail))
             {
-                _context.Student.Add(emptyCourse);
+                _context.Student.Add(newStudent);
                 await _context.SaveChangesAsync();
                 return RedirectToPage("./Index");
             }
-
-            // Select DepartmentID if TryUpdateModelAsync fails.
-            PopulateEventsDropDownList(_context, emptyCourse.StudentID);
+            PopulateAssignedEventData(_context, newStudent);
             return Page();
-
-            //************************************LINES ABOVE IS NEW
-
-            //_context.Student.Add(Student);
-            //await _context.SaveChangesAsync();
-
-            //return RedirectToPage("./Index");
         }
     }
 }
--- BPA_Tracker/Pages/Students/Edit.cshtml.cs
+++ BPA_Tracker/Pages/Students/Edit.cshtml.cs
@@ -10,7 +10,8 @@ using BPA_Tracker.Models;
 
 namespace BPA_Tracker.Pages.Students
 {
-    public class EditModel : PageModel
+
+    public class EditModel : StudentEventsPageModel
     {
         private readonly BPA_Tracker.Models.BPA_TrackerContext _context;
 
@@ -29,46 +30,115 @@ namespace BPA_Tracker.Pages.Students
                 return NotFound();
             }
 
-            Student = await _context.Student.SingleOrDefaultAsync(m => m.StudentID == id);
+            Student = await _context.Student
+                .Include(i => i.AssignEvents)
+                    .ThenInclude(i => i.Event)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.StudentID == id);
 
             if (Student == null)
             {
                 return NotFound();
             }
+
+            PopulateAssignedEventData(_context, Student);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id, string[] selectedEvents)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            _context.Attach(Student).State = EntityState.Modified;
+            var studentToUpdate = await _context.Student
+                .Include(i => i.AssignEvents)
+                    .ThenInclude(i => i.Event)
+                .FirstOrDefaultAsync(m => m.StudentID == id);
 
-            try
+            if (await TryUpdateModelAsync<Student>(
+                studentToUpdate,
+                "Student",
+                i => i.FirstName, i => i.LastName,
+                i => i.ParentEmail, i => i.ParentName,
+                i => i.ParentPhone, i => i.StudentPhone,
+                i => i.StudentEmail))
             {
+                //               if (String.IsNullOrWhiteSpace(
+                //                   studentToUpdate.AssignEvents?.))
+                //               {
+                //                   studentToUpdate.AssignEvents?.Location))
+                //= null;
+                //               }
+                UpdateStudentEvents(_context, selectedEvents, studentToUpdate);
                 await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(Student.StudentID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
-        }
-
-        private bool StudentExists(int id)
-        {
-            return _context.Student.Any(e => e.StudentID == id);
+            UpdateStudentEvents(_context, selectedEvents, studentToUpdate);
+            PopulateAssignedEventData(_context, studentToUpdate);
+            return Page();
         }
     }
+    //public class EditModel : PageModel
+    //{
+    //    private readonly BPA_Tracker.Models.BPA_TrackerContext _context;
+
+    //    public EditModel(BPA_Tracker.Models.BPA_TrackerContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    [BindProperty]
+    //    public Student Student { get; set; }
+
+    //    public async Task<IActionResult> OnGetAsync(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        Student = await _context.Student.SingleOrDefaultAsync(m => m.StudentID == id);
+
+    //        if (Student == null)
+    //        {
+    //            return NotFound();
+    //        }
+    //        return Page();
+    //    }
+
+    //    public async Task<IActionResult> OnPostAsync()
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return Page();
+    //        }
+
+    //        _context.Attach(Student).State = EntityState.Modified;
+
+    //        try
+    //        {
+    //            await _context.SaveChangesAsync();
+    //        }
+    //        catch (DbUpdateConcurrencyException)
+    //        {
+    //            if (!StudentExists(Student.StudentID))
+    //            {
+    //                return NotFound();
+    //            }
+    //            else
+    //            {
+    //                throw;
+    //            }
+    //        }
+
+    //        return RedirectToPage("./Index");
+    //    }
+
+    //    private bool StudentExists(int id)
+    //    {
+    //        return _context.Student.Any(e => e.StudentID == id);
+    //    }
+    //}
 }
